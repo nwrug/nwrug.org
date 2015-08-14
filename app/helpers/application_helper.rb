6a44a7587bc53fa82ef6f30ce23cf6a3f80161e3
Tekin Suleyman <tekin@tekin.co.uk>
2015-08-14 17:18:47 +0100

    Give pages a custom meta title and description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,27 @@
+require 'redcarpet/render_strip'
+
 module ApplicationHelper
   def mailing_list_url
     'https://groups.google.com/forum/#!forum/nwrug-members'
   end
 
   def render_markdown(markdown)
-    GitHub::Markup::Markdown.new.render(markdown).html_safe
+    markdown_renderer.render(markdown).html_safe
+  end
+
+  def render_plaintext(markdown)
+    plaintext_renderer.render(markdown)
+  end
+
+  def default_page_title
+    'North West Ruby User Group, Manchester, UK'
+  end
+
+  def markdown_renderer
+    GitHub::Markup::Markdown.new
+  end
+
+  def plaintext_renderer
+    Redcarpet::Markdown.new(Redcarpet::Render::StripDown)
   end
 end
