502a3c9e4e6e22f4ab55c2e302429632df156cf4
Tekin Suleyman <tekin@tekin.co.uk>
2015-09-23 11:06:06 +0100

    Don't share bin folder between deploys

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ set :repo_url, 'https://github.com/nwrug/nwrug.org.git'
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 set :linked_files, %w{.env}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
