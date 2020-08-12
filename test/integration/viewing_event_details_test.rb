14e8dc154ec5c0a3648e57843ceb5d4c0feeec95
Tekin Suleyman <tekin@tekin.co.uk>
2020-08-12 22:46:29 +0100

    Make sure events#index gets covered in tests

--- test/integration/viewing_event_details_test.rb
+++ test/integration/viewing_event_details_test.rb
@@ -3,7 +3,11 @@ require 'test_helper'
 class ViewingEventDetailsTest < ActionDispatch::IntegrationTest
   test "displays an event's details" do
     event = events(:august_2015)
-    visit event_path(event)
+    visit events_path
+
+    within "ul#upcoming-events" do
+      click_on event.title
+    end
 
     assert page.has_content?(event.title)
     assert page.has_content?(event.description)
