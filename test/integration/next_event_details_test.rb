09f2de029336833c5f91a01ead726c355dfa509c
Tekin Süleyman <tekin@tekin.co.uk>
2021-03-16 18:10:19 +0000

    Put NWRUG on pause

--- test/integration/next_event_details_test.rb
+++ test/integration/next_event_details_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class NextEventDetailsTest < ActionDispatch::IntegrationTest
 
   test 'homepage includes a link to the next upcoming event' do
+    skip
     event = events(:next_event)
 
     visit root_path
@@ -11,6 +12,7 @@ class NextEventDetailsTest < ActionDispatch::IntegrationTest
   end
 
   test 'homepage displays the default date (the next third Thursday) if no future event exist yet' do
+    skip
     travel_to Date.new(2040, 8, 21) do
       visit root_path
       assert page.has_content? "Next Event: Thursday 20th September"
@@ -18,6 +20,7 @@ class NextEventDetailsTest < ActionDispatch::IntegrationTest
   end
 
   test 'participate page displays the default date' do
+    skip
     travel_to Date.new(2040, 8, 21) do
       visit participate_path
       assert page.has_content? "The next event will be on Thursday 20th September"
