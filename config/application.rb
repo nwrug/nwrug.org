f00c0213418e33f1b3b37bcfbe766a13fe8f1106
Tekin Suleyman <tekin@tekin.co.uk>
2025-04-12 10:03:50 +0100

    Use Rails 6.1 framework defaults
    Nothing in the new defaults to worry about so remove the new framework
    defaults file and update application.rb to use them implicitly.

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,6 @@ Bundler.require(*Rails.groups)
 
 module Nwrug
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.0
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
