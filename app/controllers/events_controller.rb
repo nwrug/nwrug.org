d141a3176b0ca2b7f05a72d5b32f06ad1afed05e
Tekin Suleyman <tekin@tekin.co.uk>
2025-05-09 17:25:09 +0100

    Add basic ical for NWRUG events
    We include all events going back one year.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,3 +1,5 @@
+require 'icalendar'
+
 class EventsController < ApplicationController
   before_action :find_event, only: [:show, :edit, :update]
   before_action :authorise, except: [:index, :show]
@@ -5,6 +7,11 @@ class EventsController < ApplicationController
   def index
     @upcoming = Event.upcoming.includes(:location)
     @previous = Event.previous.includes(:location)
+
+    respond_to do |format|
+      format.html
+      format.ics { render plain: one_year_of_ical }
+    end
   end
 
   def new
@@ -36,5 +43,24 @@ private
 
   def event_params
     params.require(:event).permit(:title, :description, :date, :slug, :location_id, :online)
+  end
+
+
+  def one_year_of_ical
+    events = Event.where(date: 1.year.ago...).order(date: :desc).includes(:location)
+    cal = Icalendar::Calendar.new
+
+    events.each do |event|
+      cal.event do |e|
+        e.uid         = "nwrug-event-#{event.id}"
+        e.location    = "#{event.location.city}, England"
+        e.dtstart     = Icalendar::Values::DateTime.new(event.date, tzid: 'Europe/London')
+        e.dtend       = Icalendar::Values::DateTime.new((event.date+2.hours), tzid: 'Europe/London')
+        e.summary     = "NWRUG: #{event.title}"
+        e.description = event.description
+      end
+    end
+
+    cal.to_ical
   end
 end
