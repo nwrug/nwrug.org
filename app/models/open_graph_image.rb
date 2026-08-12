6d9949ec3bf10ee772ae021e75882942a6416160
Tekin Suleyman <tekin@tekin.co.uk>
2026-08-12 21:57:00 +0100

    Parameterise OpenGraphImage title and subheading

--- app/models/open_graph_image.rb
+++ app/models/open_graph_image.rb
@@ -1,5 +1,13 @@
 # Used to generate Open Graph images for NWRUG with the logo on the left and
 # text on the right.
+#
+# Example usage:
+#
+#  image = OpenGraphImage.new(
+#    title: 'North West Ruby User Group', subheading: "3rd Thursday of the month\n— Manchester, UK"
+#  ).generate
+#  File.binwrite(Rails.root.join("public/images/og-image.png"), image)
+#
 class OpenGraphImage
   WIDTH  = 1200
   HEIGHT = 630
@@ -17,6 +25,12 @@ class OpenGraphImage
   TEXT_LEFT = 570
   LOGO_LEFT  = 70
 
+  attr_reader :title, :subheading
+
+  def initialize(title:, subheading:)
+    @title = title
+    @subheading = subheading
+  end
 
   def generate
     canvas.composite(
@@ -47,12 +61,12 @@ class OpenGraphImage
 
   # @return [Vips::Image] of the title
   def title_image
-    image_from_text('North West Ruby User Group', size: TITLE_FONT_SIZE, width: TEXT_BLOCK_WIDTH, colour: TITLE_COLOUR)
+    image_from_text(title, size: TITLE_FONT_SIZE, width: TEXT_BLOCK_WIDTH, colour: TITLE_COLOUR)
   end
 
   # @return [Vips::Image] of the subheading
   def subheading_image
-    image_from_text("3rd Thursday of the month\n— Manchester, UK", size: DETAILS_FONT_SIZE, width: TEXT_BLOCK_WIDTH, colour: DETAILS_COLOUR)
+    image_from_text(subheading, size: DETAILS_FONT_SIZE, width: TEXT_BLOCK_WIDTH, colour: DETAILS_COLOUR)
   end
 
   # @return [Vips::Image] of the supplied text
