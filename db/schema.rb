6c4a7bc31025efd1454f7ea14c2988b53dfa3374
Tekin Suleyman <tekin@tekin.co.uk>
2026-08-16 09:27:53 +0100

    Dump Rails 8.1 version of schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -1,58 +1,53 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200812210327) do
-
-  create_table "events", force: :cascade do |t|
-    t.string   "title",       limit: 255
-    t.string   "slug",        limit: 255
-    t.text     "description", limit: 65535
-    t.datetime "date"
-    t.datetime "created_at",                                null: false
-    t.datetime "updated_at",                                null: false
-    t.integer  "location_id", limit: 4
-    t.boolean  "online",                    default: false
+ActiveRecord::Schema[8.1].define(version: 2020_08_12_210327) do
+  create_table "events", id: :integer, charset: "utf8mb3", collation: "utf8mb3_unicode_ci", force: :cascade do |t|
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "date", precision: nil
+    t.text "description"
+    t.integer "location_id"
+    t.boolean "online", default: false
+    t.string "slug"
+    t.string "title"
+    t.datetime "updated_at", precision: nil, null: false
+    t.index ["date"], name: "index_events_on_date"
+    t.index ["location_id"], name: "index_events_on_location_id"
   end
 
-  add_index "events", ["date"], name: "index_events_on_date", using: :btree
-  add_index "events", ["location_id"], name: "index_events_on_location_id", using: :btree
-
-  create_table "locations", force: :cascade do |t|
-    t.string   "name",           limit: 255
-    t.string   "website",        limit: 255
-    t.string   "street_address", limit: 255
-    t.string   "city",           limit: 255
-    t.string   "postal_code",    limit: 255
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+  create_table "locations", id: :integer, charset: "utf8mb3", collation: "utf8mb3_unicode_ci", force: :cascade do |t|
+    t.string "city"
+    t.datetime "created_at", precision: nil, null: false
+    t.string "name"
+    t.string "postal_code"
+    t.string "street_address"
+    t.datetime "updated_at", precision: nil, null: false
+    t.string "website"
+    t.index ["name"], name: "index_locations_on_name"
   end
 
-  add_index "locations", ["name"], name: "index_locations_on_name", using: :btree
-
-  create_table "quizzes", force: :cascade do |t|
-    t.string   "title",       limit: 255
-    t.string   "slug",        limit: 255
-    t.text     "description", limit: 65535
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+  create_table "quizzes", id: :integer, charset: "utf8mb3", collation: "utf8mb3_unicode_ci", force: :cascade do |t|
+    t.datetime "created_at", precision: nil, null: false
+    t.text "description"
+    t.string "slug"
+    t.string "title"
+    t.datetime "updated_at", precision: nil, null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "name",            limit: 255
-    t.string   "email",           limit: 255
-    t.string   "password_digest", limit: 255
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+  create_table "users", id: :integer, charset: "utf8mb3", collation: "utf8mb3_unicode_ci", force: :cascade do |t|
+    t.datetime "created_at", precision: nil, null: false
+    t.string "email"
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "updated_at", precision: nil, null: false
   end
-
 end
